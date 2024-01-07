f273d20db75ce6c702f47b2e23210226fcb3627d
hotramen-hellfire <22b0956@iitb.ac.in>
2024-01-07 14:11:47 +0530

    updaetd navbar. . .

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -21,11 +21,12 @@ const Navbar: React.FC<NavbarProps> = () => {
                 height="50px"
                 overflow={"visible"}
                 justify={'space-between'}
-                pl={10}
-                pr={10}
+                pl={{ base: 0, md: 2 }}
+                pr={{ base: 0, md: 2 }}
                 align={'center'}
                 boxShadow={'2xl'}
                 overflowY={'hidden'}
+                overflowX={'hidden'}
                 _hover={{
                     boxShadow: 'dark-lg'
                 }}
